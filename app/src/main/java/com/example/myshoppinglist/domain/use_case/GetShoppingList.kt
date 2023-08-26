fb55019faf1ad06176e114704961f338596074c2
Darius3001 <darius.mertin@outlook.com>
2023-08-26 22:10:04 +0200

    sort list by checked

--- app/src/main/java/com/example/myshoppinglist/domain/use_case/GetShoppingList.kt
+++ app/src/main/java/com/example/myshoppinglist/domain/use_case/GetShoppingList.kt
@@ -3,9 +3,14 @@ package com.example.myshoppinglist.domain.use_case
 import com.example.myshoppinglist.domain.ShoppingListRepository
 import com.example.myshoppinglist.domain.model.ShoppingListEntry
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 
 class GetShoppingList(
     private val repository: ShoppingListRepository
 ) {
-    operator fun invoke(): Flow<List<ShoppingListEntry>> = repository.getShoppingList()
+    operator fun invoke(): Flow<List<ShoppingListEntry>> = repository
+        .getShoppingList()
+        .map { list ->
+            list.sortedBy { it.checked }
+        }
 }
